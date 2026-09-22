d5490d7376a170319bf57b5d521fddc258dc6358
ShA-MS <57481825+pyraxxz@users.noreply.github.com>
2026-09-22 20:56:25 +0000

    feat(website): rebuild src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@fontsource/bricolage-grotesque/500.css';
+import '@fontsource/bricolage-grotesque/700.css';
+import '@fontsource/bricolage-grotesque/900.css';
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
 import App from './App';
 import './styles.css';
 
